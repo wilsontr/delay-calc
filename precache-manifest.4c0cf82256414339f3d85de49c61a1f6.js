7c6f6eb1dd6997745847b95b7af9efbb2552468b
Trevor Wilson <trevor@yieldstreet.com>
2019-09-04 17:11:35 -0400

    Updates

---
+++ precache-manifest.4c0cf82256414339f3d85de49c61a1f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64faca148410950b8b3e16fe78cc5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36e23cc43230f9bea0af",
+    "url": "/static/css/main.7724dae6.chunk.css"
+  },
+  {
+    "revision": "659889432d93193f1fae",
+    "url": "/static/js/2.d2adeaf4.chunk.js"
+  },
+  {
+    "revision": "36e23cc43230f9bea0af",
+    "url": "/static/js/main.c4b540ce.chunk.js"
+  },
+  {
+    "revision": "9aaeebaa5df6e3ce0525",
+    "url": "/static/js/runtime~main.a9c56d61.js"
+  }
+]);
